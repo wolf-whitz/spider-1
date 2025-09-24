cdf33f14a97c0ffece2c7647f9b8db579b57fc5d
WOLF <whitzscott@gmail.com>
2025-09-24 22:18:53 +0800

    Update and rename manifest.js to manifest.json

--- manifest.js
+++
@@ -1,7 +0,0 @@
-import spiderBato from "./spider-bato.json";
-import spiderHentai20 from "./spider-hentai20.json";
-
-export const crawlers = [
-  { id: spiderBato.spider.id, spider: spiderBato.spider },
-  { id: spiderHentai20.spider.id, spider: spiderHentai20.spider },
-];
