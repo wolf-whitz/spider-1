b688062730e162ea296f13ca06dc12ede242a74d
WOLF <whitzscott@gmail.com>
2025-09-24 22:12:04 +0800

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,8 +1,7 @@
-import { Spider } from "./types";
-import spiderMTO from "./spider-bato.json";
+import spiderBato from "./spider-bato.json";
 import spiderHentai20 from "./spider-hentai20.json";
 
-export const crawlers: Spider[] = [
-  spiderMTO.spider,
-  spiderHentai20.spider
+export const crawlers = [
+  { id: spiderBato.spider.id, spider: spiderBato.spider },
+  { id: spiderHentai20.spider.id, spider: spiderHentai20.spider },
 ];
