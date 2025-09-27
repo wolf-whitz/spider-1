5a55098e4c9d45b43caa4b7aae49b6c2a7a99c2e
WOLF <whitzscott@gmail.com>
2025-09-27 20:27:20 +0800

    updated the tapas.ts

--- src/spiders/tapas/tapas.ts
+++ src/spiders/tapas/tapas.ts
@@ -1,4 +1,4 @@
-import { createSpider, ItemConfigBuilder } from "@sdk"
+import { createSpider, ItemConfigBuilder } from "@sdk";
 
 const spider = createSpider("tapas_full")
   .name("Tapas Full Search & Episodes")
@@ -13,36 +13,61 @@ const spider = createSpider("tapas_full")
   .field("page_image", "url")
   .targetUrl("https://tapas.io/search?q={query}")
   .itemConfig((item: ItemConfigBuilder) => {
-    item.selector("div.series-list div.series-item")
-    item.Name({ selector: "p.title a strong", text: true })
-    item.Genres({ selector: "p.tag a", multiple: true })
-    item.profileLink({ selector: "p.title a", attribute: "href" })
+    item.selector(
+      "div.body div.global-page section.page-section ul.content-list-wrap li.search-item-wrap"
+    );
+
+    item.Name({ selector: "p.title a strong", text: true });
+
+    item.Genres({ selector: "p.tag a", multiple: true });
+
+    item.profileLink({ selector: "p.title a", attribute: "href" });
 
     item.profileById({
       urlPattern: "https://tapas.io/series/{manga_id}/info",
       ProfileTarget: {
-        Url: { selector: "script", parseScriptJson: true, jsonPath: "$.series.slug" },
-        Image: { selector: "script", parseScriptJson: true, jsonPath: "$.series.thumbnail" },
-        Name: { selector: "script", parseScriptJson: true, jsonPath: "$.series.title" },
-        Description: { selector: "script", parseScriptJson: true, jsonPath: "$.series.description" },
-        Genres: { selector: "script", parseScriptJson: true, jsonPath: "$.series.genres[*]", multiple: true },
-        MangaID: { selector: "script", parseScriptJson: true, jsonPath: "$.series.id" },
+        MangaID: {
+          selector: "script",
+          parseScriptJson: true,
+          jsonPath: "$.series.id",
+        },
+        Name: {
+          selector: "script",
+          parseScriptJson: true,
+          jsonPath: "$.series.title",
+        },
+        Image: {
+          selector: "script",
+          parseScriptJson: true,
+          jsonPath: "$.series.thumbnail",
+        },
+        Description: {
+          selector: "script",
+          parseScriptJson: true,
+          jsonPath: "$.series.description",
+        },
+        Genres: {
+          selector: "script",
+          parseScriptJson: true,
+          jsonPath: "$.series.genres[*]",
+          multiple: true,
+        },
         Chapters: {
-          selector: `https://tapas.io/series/{manga_id}/episodes?page=1`,
+          selector: "https://tapas.io/series/{manga_id}/episodes?page=1",
           parseScriptJson: true,
           jsonPath: "$.data.episodes[*]",
           multiple: true,
-          arranger: "newestFirst"
+          arranger: "newestFirst",
         },
         PageImage: {
           arrayVar: {
-            variableNames: ["thumb_url"], 
-            matchPattern: "https://", 
-            scriptMatch: "thumb_url"
-          }
-        }
-      }
-    })
-  })
+            variableNames: ["thumb_url"],
+            matchPattern: "https://",
+            scriptMatch: "thumb_url",
+          },
+        },
+      },
+    });
+  });
 
-spider.save()
+spider.save();
