bd54ab33a6a2f01615726978fe126385fc56746a
whitzscott <whitzscott@gmail.com>
2025-11-23 14:52:14 +0800

    ad

--- src/spiders/yaksha/yaksha.ts
+++ src/spiders/yaksha/yaksha.ts
@@ -17,27 +17,15 @@ const spider = createSpider("yaksha_full")
     item.profileLink({ selector: ".c-tabs-item__content a", attribute: "href" });
 
     item.profileTarget(pt => {
+      pt.fetch(true);
       pt.Url({ selector: "head link[rel='canonical']", attribute: "href" });
       pt.Name({ selector: ".post-title h1", text: true });
       pt.Image({ selector: ".summary_image img", attribute: "src" });
       pt.Description({ selector: ".description-summary", text: true });
-      pt.Genres({ selector: ".genres-content a", multiple: true, text: true });
+      pt.Genres({ selector: ".post-content_item .genres-content a", multiple: true, text: true });
       pt.Chapters({ selector: ".listing-chapters_wrap ul.main.version-chap li.wp-manga-chapter a", multiple: true, text: true });
       pt.PageImage({ selector: ".reading-content .wp-manga-chapter-img", multiple: true, attribute: "src" });
-    });
-
-    item.profileById({
-      urlPattern: "https://yakshascans.com/manga/{manga_id}/",
-      fetch: true,
-      ProfileTarget: {
-        Url: { selector: "head link[rel='canonical']", attribute: "href" },
-        Image: { selector: ".summary_image img", attribute: "src" },
-        Name: { selector: ".post-title h1", text: true },
-        Description: { selector: ".description-summary", text: true },
-        Genres: { selector: ".genres-content a", multiple: true, text: true },
-        Chapters: { selector: ".listing-chapters_wrap ul.main.version-chap li.wp-manga-chapter a", multiple: true, text: true },
-        PageImage: { selector: ".reading-content .wp-manga-chapter-img", multiple: true, attribute: "src" },
-      },
+      pt.MangaID({ selector: ".post-title h1", text: true });
     });
   });
 
